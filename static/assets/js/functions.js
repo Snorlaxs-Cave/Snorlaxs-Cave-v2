f0854870880008e52e153ebfa32f76fa74dbff95
snorlax <143361136+snorlaxscave@users.noreply.github.com>
2024-01-18 23:25:21 +0000

    Update functions.js

--- static/assets/js/functions.js
+++ static/assets/js/functions.js
@@ -45,3 +45,9 @@ function apps(url) {
       document.title = newTabName;
       localStorage.setItem('tabName', newTabName);
     }
+function cloak() {
+    let url = window.location.href;
+    var w = window.open("about:blank", "_blank");
+    w.document.write('<iframe style="position: absolute;top: 0px;bottom: 0px;right: 0px;width: 100%;border: none;margin: 0;padding: 0;overflow: hidden;z-index: 99999;height: 100%;" src="' + url + '"></iframe>');
+    window.close('', '_parent', '');
+}
